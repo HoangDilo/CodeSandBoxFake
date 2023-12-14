c7c0ad3d76d0e4ba9adfd8da95d709d745e44783
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-12-14 09:07:15 +0700

    Delete src directory

--- src/components/CustomFileExplorer/CustomFileExplorer.tsx
+++
@@ -1,9 +0,0 @@
-import { Sandpack } from "@codesandbox/sandpack-react"
-import { useContext } from "react"
-
-export default function CustomFileExplorer() {
-
-  return (
-    <div>CustomFileExplorer</div>
-  )
-}
